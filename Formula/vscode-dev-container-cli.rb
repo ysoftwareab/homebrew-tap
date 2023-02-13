da53d5cb4e77b6293d070679c579dd58e55477b2
Andrei Neculau <andrei.neculau@gmail.com>
2023-02-13 18:08:20 +0100

    bump @vscode/dev-container-cli to 0.268.0

--- Formula/vscode-dev-container-cli.rb
+++ Formula/vscode-dev-container-cli.rb
@@ -3,8 +3,8 @@ class VscodeDevContainerCli < Formula
 
   desc "Command Line Interface (CLI) for VS Code Dev Containers"
   homepage "https://code.visualstudio.com/docs/remote/devcontainer-cli"
-  url "https://registry.npmjs.org/@vscode/dev-container-cli/-/dev-container-cli-0.217.1.tgz"
-  sha256 "2bc0f2887af01bc674f7ebc29183bc73d8a4162e6ee9c5394c2c903f3c04e99e"
+  url "https://registry.npmjs.org/@vscode/dev-container-cli/-/dev-container-cli-0.268.0.tgz"
+  sha256 "c6177f0ea63d0dbe56c45a5d665cd5cfddda451ea2387c9b10d0153a304382b0"
   license "CC-BY-4.0"
 
   livecheck do
