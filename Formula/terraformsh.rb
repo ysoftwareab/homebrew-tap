b66116ba42cb19597c0337cd920b34d091b40284
Andrei Neculau <andrei.neculau@gmail.com>
2023-03-26 12:52:24 +0200

    fix sha

--- Formula/terraformsh.rb
+++ Formula/terraformsh.rb
@@ -2,7 +2,7 @@ class Terraformsh < Formula
   desc "A wrapper for Terraform in Bash"
   homepage "https://github.com/pwillis-els/terraformsh"
   url "https://github.com/pwillis-els/terraformsh/archive/refs/tags/v0.13.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "a785d8597df71c669d91247bd674bdc57b426cdde648d5af36fd834787b8fe59"
   license "MIT"
   head "https://github.com/pwillis-els/terraformsh.git", branch: "master"
   
