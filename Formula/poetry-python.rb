341e59c16b24fb41587528c6c752d07b7ce28c1b
Andrei Neculau <andrei.neculau@gmail.com>
2024-01-18 22:55:24 +0100

    Update poetry-python.rb

--- Formula/poetry-python.rb
+++ Formula/poetry-python.rb
@@ -1,8 +1,8 @@
 class PoetryPython < Formula
   desc "A shebang for installing poetry deps before running a python script"
   homepage "https://github.com/ysoftwareab/homebrew-tap"
-  url "https://github.com/ysoftwareab/homebrew-tap/archive/refs/tags/poetry-python-1.0.tar.gz"
-  sha256 "04af757a634c57ca054472dff2f10b369d42522ae1bad258f7e9dfe44597e4cd"
+  url "https://github.com/ysoftwareab/homebrew-tap/archive/refs/tags/poetry-python-1.1.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license :public_domain
   head "https://github.com/ysoftwareab/homebrew-tap.git", branch: "master"
 
