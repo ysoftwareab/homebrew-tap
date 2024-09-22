09e52be96cd71a62d3af8b23692448c0b45137e4
Andrei Neculau <andrei.neculau@gmail.com>
2024-09-22 22:02:46 +0200

    Create backstage-entity-validator.rb

---
+++ Formula/backstage-entity-validator.rb
@@ -0,0 +1,26 @@
+class BackstageEntityValidator < Formula
+  require "language/node"
+
+  desc "Backstage entity validator"
+  homepage "https://github.com/RoadieHQ/backstage-entity-validator"
+  url "https://registry.npmjs.org/@roadiehq/backstage-entity-validator/-/backstage-entity-validator-0.5.1.tgz"
+  sha256 "273b3a33457f7b23aba151e7b63f34eae406e93aa0c11d224a5e5589aafd8b55"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://registry.npmjs.org/@roadiehq/backstage-entity-validator/latest"
+    regex(/["']version["']:\s*?["']([^"']+)["']/i)
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system "#{bin}/bev", "--help"
+    assert_predicate testpath/"package.json", :exist?, "package.json must exist"
+  end
+end
