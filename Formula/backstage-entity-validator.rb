9fca45fffe45122772a5061afe3d4804ea010fb7
Andrei Neculau <andrei.neculau@gmail.com>
2024-09-22 22:10:01 +0200

    Update backstage-entity-validator.rb

--- Formula/backstage-entity-validator.rb
+++ Formula/backstage-entity-validator.rb
@@ -16,7 +16,8 @@ class BackstageEntityValidator < Formula
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    # bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install_symlink "#{libexec}/bin/validate-entity" => "backstage-entity-validator"
   end
 
   test do
