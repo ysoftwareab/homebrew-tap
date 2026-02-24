dd949c1739325f6c7b7a41f0fd2b2b3160805883
Andrei Neculau <andrei.neculau@gmail.com>
2026-02-24 13:09:22 +0100

    Revert "version-refresh for non-node formulas"
    This reverts commit 7a99b27f9a003106d6d059c4085243dec592e4cb.

--- Formula/buildevents.rb
+++ Formula/buildevents.rb
@@ -3,34 +3,31 @@
 class Buildevents < Formula
   desc "CLI for Honeycomb build events"
   homepage "https://github.com/honeycombio/buildevents"
-  version "0.18.0"
+  version "0.9.2"
 
   if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/honeycombio/buildevents/releases/download/v0.18.0/buildevents-darwin-arm64"
-      sha256 "aec6252b26b86d0577fef0dd931445a7754a81c114ea1921a3b50103e1c787f3"
-    elsif Hardware::CPU.is_64_bit?
-      url "https://github.com/honeycombio/buildevents/releases/download/v0.18.0/buildevents-darwin-amd64"
-      sha256 "1c65dd04fe978d17ffd20387bd65288bb4d8979d3271143b576be90f38e699a7"
+    if Hardware::CPU.is_64_bit?
+      url "https://github.com/honeycombio/buildevents/releases/download/v0.9.2/buildevents-darwin-amd64"
+      sha256 "4d736f177d68eadc2a683a47c58c87f9aa0686179a264c2d3d797ede73a872b9"
     else
-      url "https://github.com/honeycombio/buildevents/releases/download/v0.18.0/buildevents-darwin-386"
+      url "https://github.com/honeycombio/buildevents/releases/download/v0.9.2/buildevents-darwin-386"
       sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/honeycombio/buildevents/releases/download/v0.18.0/buildevents-linux-amd64"
-        sha256 "fad862360932679e265b9bab2cf9e80918ac8c827a1042c78f05fd01806c7a3b"
+        url "https://github.com/honeycombio/buildevents/releases/download/v0.9.2/buildevents-linux-amd64"
+        sha256 "81d2accfeb0b7444c805d5ad90db2f14196f29900bd909ffec61648c4c479613"
       else
-        url "https://github.com/honeycombio/buildevents/releases/download/v0.18.0/buildevents-linux-386"
-        sha256 "c7bac4d81ce44a6bac94f2b6763085b1d99e976bc10e8c28fab0b20f778e76d9"
+        url "https://github.com/honeycombio/buildevents/releases/download/v0.9.2/buildevents-linux-386"
+        sha256 "21196bccd31bc464f7380accecf712e67846ec4936e5e0b50048cf0bd1034f27"
       end
     elsif Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/honeycombio/buildevents/releases/download/v0.18.0/buildevents-linux-arm64"
-        sha256 "2301f1c237962314a837244c548ee4cbc78dff9f5a357ba6d8d00baacd77e118"
+        url "https://github.com/honeycombio/buildevents/releases/download/v0.9.2/buildevents-linux-arm64"
+        sha256 "3ddfd80ce3c01e6f3f446ffe914b3c5b9b2149f7d5028a6d2d5375481682ea97"
       else
-        url "https://github.com/honeycombio/buildevents/releases/download/v0.18.0/buildevents-linux-arm"
+        url "https://github.com/honeycombio/buildevents/releases/download/v0.9.2/buildevents-linux-arm"
         sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
       end
     end
